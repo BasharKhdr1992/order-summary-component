bffc4cd8e01bc0013f434bbd099d7a445517f863
bashar khdr <bashar.khdr.DS.92@hotmail.com>
2022-04-23 03:02:12 +0200

    adding a mobile background

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import OrderSummary from './components/OrderSummary';
+import assets from './assets';
 
 const App = () => {
+  const [bg, setBg] = useState(assets.bgDesktop);
+
+  const updateBackground = (e) => {
+    if (e.currentTarget.innerWidth < 700) {
+      setBg(assets.bgMobile);
+    } else {
+      setBg(assets.bgDesktop);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener('resize', updateBackground);
+
+    return () => window.removeEventListener('resiz', updateBackground);
+  });
+
   return (
-    <div className="wrapper">
+    <div className="wrapper" style={{ backgroundImage: `url(${bg})` }}>
       <OrderSummary />
-      <div class="attribution">
+      <div className="attribution">
         Challenge by
         <a
           href="https://www.frontendmentor.io?ref=challenge"
--- src/assets.js
+++ src/assets.js
@@ -1,8 +1,12 @@
 import iconMusic from './assets/icon-music.svg';
 import hero from './assets/illustration-hero.svg';
+import bgDesktop from './assets/pattern-background-desktop.svg';
+import bgMobile from './assets/pattern-background-mobile.svg';
 
 const assets = {
   iconMusic: iconMusic,
+  bgDesktop: bgDesktop,
+  bgMobile: bgMobile,
   hero: hero,
 };
 
